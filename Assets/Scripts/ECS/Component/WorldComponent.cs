e54f83bab993a4b52e2469aad61f9114480803d3
이성희 <the.paper@live.co.kr>
2023-01-07 15:29:42 +0900

    WorldSystem 구현

--- Assets/Scripts/ECS/Component/WorldComponent.cs
+++ Assets/Scripts/ECS/Component/WorldComponent.cs
@@ -7,4 +7,7 @@ public struct WorldComponent : IComponentData
 {
     public Entity playerPrefab;
     public Entity enemyPrefab;
+
+    public bool isSpawnedPlayer;
+    public bool isSpawnedEnemy;
 }
--- Assets/Scripts/ECS/System/WorldSystem.cs
+++ Assets/Scripts/ECS/System/WorldSystem.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
 
-public struct WorldSystem : ISystem
-{
-    void ISystem.OnCreate(ref SystemState state)
+public partial struct WorldSystem : ISystem
+{   
+    public void OnCreate(ref SystemState state)
     {
-        // throw new System.NotImplementedException();
     }
 
-    void ISystem.OnDestroy(ref SystemState state)
+    public void OnDestroy(ref SystemState state)
     {
-        // throw new System.NotImplementedException();
     }
 
-    void ISystem.OnUpdate(ref SystemState state)
+    public void OnUpdate(ref SystemState state)
     {
-        // throw new System.NotImplementedException();
+        foreach (RefRW<WorldComponent> worldComponent in SystemAPI.Query<RefRW<WorldComponent>>())
+        {
+            // if (worldComponent.ValueRO.isSpawnedEnemy == false)
+            // {
+            //     Entity newEntity = state.EntityManager.Instantiate(worldComponent.ValueRO.enemyPrefab);
+            //     state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(float3.zero));
+            //     worldComponent.ValueRW.isSpawnedEnemy = true;
+            // }
+            if (worldComponent.ValueRO.isSpawnedPlayer == false)
+            {
+                Entity newEntity = state.EntityManager.Instantiate(worldComponent.ValueRO.playerPrefab);
+                state.EntityManager.SetComponentData(newEntity, new LocalTransform(){ Position = Vector3.zero, Scale = 1f, Rotation = Quaternion.Euler(0f, 180f, 0f) });
+                worldComponent.ValueRW.isSpawnedPlayer = true;
+            }
+        }
     }
 }
